724f5be479ecc60cd0b5d6a187418eea993b4152
havvlok <112236808+havvlok@users.noreply.github.com>
2024-11-16 17:46:07 +0530

    Time: 20 ms (36.89%), Space: 57.8 MB (51.6%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -3,7 +3,6 @@ class Solution {
         HashMap<Integer, Integer> h = new HashMap<>();
         for(int i = 0; i < nums.length; i++){
             if(h.containsKey(nums[i])){
-                System.out.println(h.get(nums[i]) - i);
                 if(i - h.get(nums[i]) <= k){
                     
                     return true;
